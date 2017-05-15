3894a34a83cb078467e7bd848ece5976be429406
slaweko72 <slawomirobara@gmail.com>
2017-05-15 15:19:39 +0200

    Dostawienie daty
    17.05.15

--- jtest02/src/pl/slasoft/test/NewTest.java
+++ jtest02/src/pl/slasoft/test/NewTest.java
@@ -5,6 +5,7 @@ public class NewTest {
 	public static void main(String[] args) {
 		System.out.println("123");
 		System.out.println("i jescze jedna linia!!!");
+		System.out.println("2017-05-15");
 	}
 
 }
