600f03739fe3185d695e561bb4802d4eb3f11bf6
slaweko72 <slawomirobara@gmail.com>
2017-05-16 14:44:06 +0200

    aktualizacja 05.16

--- jtest02/src/pl/slasoft/test/NewTest.java
+++ jtest02/src/pl/slasoft/test/NewTest.java
@@ -5,7 +5,7 @@ public class NewTest {
 	public static void main(String[] args) {
 		System.out.println("123");
 		System.out.println("i jescze jedna linia!!!");
-		System.out.println("2017/05/15, maj 15");
+		System.out.println("2017/05/16, maj 16");
 	}
 
 }
