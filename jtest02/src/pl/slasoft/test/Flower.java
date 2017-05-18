bb5dad7a71338ba30472dca6cfe023c0ea943069
Slawek Obara <slawomir.obara@hatomi.pl>
2017-05-18 10:58:26 +0200

    Flower class

---
+++ jtest02/src/pl/slasoft/test/Flower.java
@@ -0,0 +1,22 @@
+package pl.slasoft.test;
+
+public class Flower {
+	
+	private String name;
+	private String colour;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	
+	
+
+}
